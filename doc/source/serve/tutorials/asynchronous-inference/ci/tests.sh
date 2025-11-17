5d666561e8feb1240c6a235a66e10416a72d2861
harshit <harshit@anyscale.com>
2025-11-17 13:47:44 +0000

    fix release tests
    Signed-off-by: harshit <harshit@anyscale.com>

--- doc/source/serve/tutorials/asynchronous-inference/ci/tests.sh
+++ doc/source/serve/tutorials/asynchronous-inference/ci/tests.sh
@@ -2,7 +2,9 @@
 set -euxo pipefail
 
 # Example: convert and run a notebook
-python ci/nb2py.py content/asynchronous-inference.ipynb --out /tmp/asynchronous-inference.py
+python ci/nb2py.py content/asynchronous-inference.ipynb /tmp/asynchronous-inference.py --ignore-cmds
 python /tmp/asynchronous-inference.py
 
 python content/client.py
+
+rm /tmp/asynchronous-inference.py
